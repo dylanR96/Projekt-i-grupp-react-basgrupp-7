65069eba1f5455a5fb5a98748dbc02b1ac7502ab
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-04-24 12:26:02 +0200

    Added react contect and added some examples of usage.

--- src/pages/Add-Card/AddCard.jsx
+++ src/pages/Add-Card/AddCard.jsx
@@ -1,8 +1,12 @@
 import { useNavigate } from "react-router";
 import "./addCard.css";
+import { useContext } from "react";
+import { CardsContext } from "../../App";
 
 function AddCard() {
   const navigate = useNavigate();
+  const { cardsArray, setCardsArray } = useContext(CardsContext);
+
   return (
     <div>
       <h1>Add new card</h1>
--- src/pages/Cards-page/Cards.jsx
+++ src/pages/Cards-page/Cards.jsx
@@ -1,12 +1,25 @@
 import { useNavigate } from "react-router";
 import "./cards.css";
+import { useContext } from "react";
+import { CardsContext } from "../../App";
 
 function Cards() {
+  // ** Using global statement
+  const { cardsArray, setCardsArray } = useContext(CardsContext);
+
   const navigate = useNavigate();
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={() => navigate("/addCard")}>Add new card</button>
+      <button
+        // ** Example of usage ↓
+        onClick={() => {
+          setCardsArray([...cardsArray, 2]);
+          navigate("/addCard");
+        }}
+      >
+        Add new card
+      </button>
     </div>
   );
 }
