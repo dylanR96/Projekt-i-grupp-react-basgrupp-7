f715ffd548dc7f6bb91de312662f2e6d2e69878e
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-04-29 14:37:23 +0200

    Merge

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import "./card.css";
-import cardImage from '../../images/card.png';
-import bitcoinImage from '../../images/Bitcoin.png';
-/* import InfoCard from "./components/Form/Form"; */
-
-
-
-
-function Card() {
-  return (
-/* GLÖM EJ ATT LÄGGA IN ALEX props IGEN. glöm ej att card är inlagt i addCrds också */
-  <>
-  <div className="card">
-    <section className='card_img'>
-        <img src={cardImage} alt="" className="payment"/>
-        <img src={bitcoinImage} alt="" className="bitcoin" />
-    </section>
-      <h2 className="card_numbers">XXXX XXXX XXXX XXXX </h2>
-    <section className="card_section_1">
-      <div>
-        <p>CARDHOLDER NAME: </p>
-        <p>xxxx xxxx</p>
-      </div>
-      <div>
-        <p>VALID THRU: </p>
-        <p>xxxx xxxx</p>
-      </div>
-    </section>
-  </div>
-</>
-)
-=======
 import styles from "./card.module.css";
 
 function Card({ card }) {
@@ -79,30 +46,6 @@ function Card({ card }) {
       </div>
     </article>
   );
->>>>>>> e-wallet-lucas
 }
 
 export default Card;
-
-
-/* 
-
-function Card() {
-  return (
-
-  <>
-  <div className='card_img'>
-      <img src={cardImage} alt="" />
-  </div>
-    <h2 className="card_numbers">Card Number: {card.cardNumber}</h2>
-  <div>
-      <p>Cardholder Name: {card.cardholderName}</p>
-      <p>Valid Thru: {card.validThru}</p>
-  </div>
-  <div>
-    <p>CCV: {card.ccv}</p>  
-    <p>Vendor: {card.vendor}</p>
-  </div>
-
-</> */
-
